1327fa7aeaf107c9c937e86d4a42920506a7570f
Ardiyansyah R <arsk9797@gmail.com>
2023-11-14 02:07:18 +0800

    membuat testing api create car

--- db/seeders/20220528064914-users.js
+++ db/seeders/20220528064914-users.js
@@ -1,42 +1,42 @@
-'use strict';
+"use strict"
 
-const { Op } = require("sequelize");
-const bcrypt = require("bcryptjs");
-const { Role } = require("../../app/models");
+const { Op } = require("sequelize")
+const bcrypt = require("bcryptjs")
+const { Role } = require("../../app/models")
 
-const names = [
-  "Johnny",
-  "Fikri",
-  "Brian",
-  "Ranggawarsita",
-  "Jayabaya",
-]
+const names = ["Johnny", "Fikri", "Brian", "Ranggawarsita", "Jayabaya", "AncR"]
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const password = "123456";
-    const encryptedPassword = bcrypt.hashSync(password, 10);
-    const timestamp = new Date();
+  async up(queryInterface, Sequelize) {
+    const password = "123456"
+    const encryptedPassword = bcrypt.hashSync(password, 10)
+    const timestamp = new Date()
 
-    const role = await Role.findOne({
+    const customerRole = await Role.findOne({
       where: {
         name: "CUSTOMER",
-      }
+      },
+    })
+
+    const adminRole = await Role.findOne({
+      where: {
+        name: "ADMIN",
+      },
     })
 
     const users = names.map((name) => ({
       name,
       email: `${name.toLowerCase()}@binar.co.id`,
       encryptedPassword,
-      roleId: role.id, 
+      roleId: name === "AncR" ? adminRole.id : customerRole.id,
       createdAt: timestamp,
       updatedAt: timestamp,
     }))
 
-    await queryInterface.bulkInsert('Users', users, {});
+    await queryInterface.bulkInsert("Users", users, {})
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', { name: { [Op.in]: names } }, {});
-  }
-};
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", { name: { [Op.in]: names } }, {})
+  },
+}
--- tests/Car.api.test.js
+++ tests/Car.api.test.js
@@ -1,8 +1,8 @@
-const request = require('supertest');
-const app = require('../app')
+const request = require("supertest")
+const app = require("../app")
 // const baseURL = "http://localhost:8000"
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require("dotenv")
+dotenv.config()
 
 // describe("Dog", () => {
 //     it("should have name called 'Arnold'", () => {
@@ -26,15 +26,67 @@ dotenv.config();
 // });
 
 describe("API get all cars", () => {
-    it("success get all data cars", async () => {
-        const response = await request(app).get('/v1/cars')
-        expect(response.statusCode).toBe(200);
-    });
-});
+  it("success get all data cars", async () => {
+    const response = await request(app).get("/v1/cars")
+    expect(response.statusCode).toBe(200)
+  })
+})
 
 describe("API get car By ID", () => {
-    it("success get data car", async () => {
-        const response = await request(app).get('/v1/cars/20')
-        expect(response.statusCode).toBe(200);
-    });
-});
+  it("success get data car", async () => {
+    const response = await request(app).get("/v1/cars/20")
+    expect(response.statusCode).toBe(200)
+  })
+})
+
+// created by R
+describe("API Create Car", () => {
+  it("success create car", async () => {
+    const admin = {
+      email: "ancr@binar.co.id",
+      password: "123456",
+    }
+    const check = await request(app).post("/v1/auth/login").send(admin)
+    const res = JSON.parse(check.text)
+    const token = res.accessToken
+    const car = {
+      name: "Mustang GT",
+      price: 2000,
+      size: "SMALL",
+      image:
+        "https://tse2.mm.bing.net/th?id=OIP.U2iQ7wNK6ZzTW_traW_-PQHaHa&pid=Api&P=0&h=180",
+      isCurrentlyRented: false,
+    }
+
+    const response = await request(app)
+      .post("/v1/cars")
+      .set(`Authorization`, `Bearer ${token}`)
+      .send(car)
+
+    expect(response.statusCode).toBe(201)
+  })
+
+  it("failed create car : Access forbidden", async () => {
+    const admin = {
+      email: "ancr@binar.co.id",
+      password: "1234567",
+    }
+    const check = await request(app).post("/v1/auth/login").send(admin)
+    const res = JSON.parse(check.text)
+    const token = res.accessToken
+    const car = {
+      name: "Mustang GT",
+      price: 2000,
+      size: "SMALL",
+      image:
+        "https://tse2.mm.bing.net/th?id=OIP.U2iQ7wNK6ZzTW_traW_-PQHaHa&pid=Api&P=0&h=180",
+      isCurrentlyRented: false,
+    }
+    const response = await request(app)
+      .post("/v1/cars")
+      .set(`Authorization`, `Bearer ${token}`)
+      .send(car)
+
+    expect(response.statusCode).toBe(401)
+  })
+})
