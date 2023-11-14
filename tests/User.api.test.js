c64d0b631eaec7de37c7a268107b278551658cfd
NARajab <11211067@student.itk.ac.id>
2023-11-15 01:08:48 +0800

    modified:   app/router.js 	modified:   db/seeders/20220528064914-users.js 	modified:   tests/Car.api.test.js 	modified:   tests/User.api.test.js

--- tests/User.api.test.js
+++ tests/User.api.test.js
@@ -9,60 +9,6 @@ describe("API Login", () => {
       email: "fikri@binar.co.id",
       password: "123456",
     };
-    const request = require("supertest");
-    const dotenv = require("dotenv");
-    const app = require("../app");
-
-    dotenv.config();
-
-    describe("API Login", () => {
-      it("success login", async () => {
-        const user = {
-          email: "fikri@binar.co.id",
-          password: "123456",
-        };
-        const response = await request(app).post("/v1/auth/login").send(user);
-        expect(response.statusCode).toBe(201);
-      });
-
-      it("failed login : wrong password", async () => {
-        const failedUser = {
-          email: "fikri@binar.co.id",
-          password: "1234656",
-        };
-        const response = await request(app)
-          .post("/v1/auth/login")
-          .send(failedUser);
-        expect(response.statusCode).toBe(401);
-      });
-    });
-
-    describe("API Register", () => {
-      it("success register", async () => {
-        const user = {
-          name: "membertest4",
-          email: "membertest4@binar.co.id",
-          password: "123456",
-        };
-        const response = await request(app)
-          .post("/v1/auth/register")
-          .send(user);
-        expect(response.statusCode).toBe(201);
-      });
-
-      it("failed register: email already taken", async () => {
-        const existingUser = {
-          name: "membertest4",
-          email: "membertest4@binar.co.id",
-          password: "1234567",
-        };
-        const response = await request(app)
-          .post("/v1/auth/register")
-          .send(existingUser);
-        expect(response.statusCode).toBe(500);
-      });
-    });
-
     const response = await request(app).post("/v1/auth/login").send(user);
     expect(response.statusCode).toBe(201);
   });
@@ -74,6 +20,30 @@ describe("API Login", () => {
     };
     const response = await request(app).post("/v1/auth/login").send(failedUser);
     expect(response.statusCode).toBe(401);
+  });
+});
+
+describe("API Register", () => {
+  it("success register", async () => {
+    const user = {
+      name: "membertest4",
+      email: "membertest4@binar.co.id",
+      password: "123456",
+    };
+    const response = await request(app).post("/v1/auth/register").send(user);
+    expect(response.statusCode).toBe(201);
+  });
+
+  it("failed register: email already taken", async () => {
+    const existingUser = {
+      name: "membertest4",
+      email: "membertest4@binar.co.id",
+      password: "1234567",
+    };
+    const response = await request(app)
+      .post("/v1/auth/register")
+      .send(existingUser);
+    expect(response.statusCode).toBe(500);
   });
 });
 
